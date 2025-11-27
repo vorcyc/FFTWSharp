9c8802d5f0f24a375cd3ec69e1fe6adeb9c6673a
cyclone_dll <cyclone_dll@qq.com>
2025-11-27 13:24:03 +0800

    Add project files.

---
+++ FFTWSharp/PinnableArrayExtension.cs
@@ -0,0 +1,81 @@
+﻿using Vorcyc.Mathematics;
+using Vorcyc.Mathematics.Numerics;
+using System.Numerics;
+
+namespace FFTWSharp;
+
+public static class PinnableArrayExtension
+{
+   
+    extension (PinnableArray<float> array)
+    {
+        /// <summary>
+        /// Scales all elements of the array in place so that their sum equals 1.
+        /// </summary>
+        /// <remarks>This method normalizes the array by dividing each element by the total number of
+        /// elements. After calling this method, the sum of all elements in the array will be 1, provided the array
+        /// contains numeric values. If the array is empty, no scaling is performed.</remarks>
+        public void ScaleInPlace()
+        {
+            var factor = 1.0f / array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= factor;
+            }
+        }
+    }
+
+    extension (PinnableArray<double> array)
+    {
+        /// <summary>
+        /// Scales the elements of the array in place so that their sum equals 1.
+        /// </summary>
+        /// <remarks>This method normalizes the array by dividing each element by the total number of
+        /// elements. After calling this method, the sum of all elements in the array will be 1, provided the array
+        /// contains numeric values. If the array is empty, no scaling is performed.</remarks>
+        public void ScaleInPlace()
+        {
+            var factor = 1.0 / array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= factor;
+            }
+        }
+    }
+
+    extension (PinnableArray<ComplexFp32> array)
+    {
+        /// <summary>
+        /// Scales the elements of the array in place so that their sum equals 1.
+        /// </summary>
+        /// <remarks>This method normalizes the array by dividing each element by the total number of
+        /// elements. After calling this method, the sum of all elements in the array will be 1, unless the array is
+        /// empty or contains non-numeric values such as NaN or Infinity.</remarks>
+        public void ScaleInPlace()
+        {
+            var factor = 1.0f / array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= factor;
+            }
+        }
+    }
+
+    extension (PinnableArray<Complex> array)
+    {
+        /// <summary>
+        /// Scales the elements of the underlying array in place so that their sum equals 1.
+        /// </summary>
+        /// <remarks>This method modifies the array directly by multiplying each element by a
+        /// normalization factor. If the array is empty, no changes are made. Use this method when you need to normalize
+        /// the array values without creating a new array.</remarks>
+        public void ScaleInPlace()
+        {
+            var factor = 1.0 / array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= factor;
+            }
+        }
+    }
+}
---
+++ TEST/Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FFTWSharp;
+using FFTWSharp.FftwSingle;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using Vorcyc.Mathematics;
+using Vorcyc.Mathematics.Framework.Utilities;
+using Vorcyc.Mathematics.Numerics;
+using Vorcyc.Mathematics.SignalProcessing.Fourier;
+using Vorcyc.Mathematics.SignalProcessing.Transforms;
+
+PinnableArray<float> r = new(1024, true);
+r.Fill(0, 1);
+PinnableArray<ComplexFp32> c = new(1024, true);
+c.Fill(0, new ComplexFp32(1, 0));
+
+r.PrintLine(ConsoleColor.Yellow);
+c.PrintLine(ConsoleColor.Cyan);
+
+FFTWSharp.FftwSingle.Dft1D.Dft1DComplexInPlace(c, FFTWSharp.Interop.fftw_direction.Forward);
+c.PrintLine(ConsoleColor.Green);
+
+FFTWSharp.FftwSingle.Dft1D.Dft1DComplexInPlace(c, FFTWSharp.Interop.fftw_direction.Backward);
+
+c.ScaleInPlace();
+c.PrintLine(ConsoleColor.Magenta);
+
+
+
+FFTWSharp.FftwSingle.Dft1D.Dft1DR2C(r, c);
+c.PrintLine();
+
+FFTWSharp.FftwSingle.Dft1D.Dft1DC2R(c, r);
+r.PrintLine(ConsoleColor.Green);
+
+
+
+
+
+
+//BenchmarkRunner.Run<FFT_benchmark>();
+
+
+public class FFT_benchmark
+{
+
+
+    [Params(16384, 32768, 65536, 131072, 262144, 524288, 4194304, 16777216)]
+    public int N;
+
+
+    //public float[] _array;
+    private PinnableArray<float>? _array;
+    private PinnableArray<ComplexFp32>? _complex;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        //_array = new float[N];
+        //for (int i = 0; i < _array.Length; i++)
+        //{
+        //    _array[i] = Random.Shared.NextSingle();
+        //}
+        _array?.Dispose();
+        _array = null;
+        _array = new(N, true);
+
+        _complex?.Dispose();
+        _complex = null;
+        _complex = new(N, true);
+
+        FastFourierTransform.Version = FftVersion.SIMD;
+    }
+
+
+    [Benchmark]
+    public bool my_forward() => FastFourierTransform.Forward(_array, _complex);
+
+
+    [Benchmark]
+    public bool my_backward() => FastFourierTransform.Inverse(_complex, _complex);
+
+
+    [Benchmark]
+    public void fftw_forward() => FFTWSharp.FftwSingle.Dft1D.Dft1DR2C(_array, _complex);
+
+
+    [Benchmark]
+    public void fftw_backward() => FFTWSharp.FftwSingle.Dft1D.Dft1DC2R(_complex, _array);
+
+
+
+
+
+
+
+}
